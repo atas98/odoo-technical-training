03910c7d75d1af5c71f69287f899db3442cda4bd
atas <andy.torkin@gmail.com>
2021-04-23 17:41:11 +0300

    Added relational fields to library.rental for additional info

--- library/models/rental.py
+++ library/models/rental.py
@@ -9,3 +9,11 @@ class Rental(models.Model):
     book_id = fields.Many2one('library.book', 'rental_ids')
     rental_date = fields.Date()
     return_date = fields.Date()
+
+    customer_address = fields.Text(related='customer_id.address', stored=True)
+    customer_email = fields.Char(related='customer_id.email', stored=True)
+    book_authors = fields.Many2many(related='book_id.author_ids', stored=True)
+    book_edition_date = fields.Date(related='book_id.edition_date',
+                                    stored=True)
+    book_publisher = fields.Char(related='book_id.publisher_id.name',
+                                 stored=True)
